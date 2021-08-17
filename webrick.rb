aa241b907325d9d592b15d92f501a0de9e455f53
katonaoya <kato7080220@yahoo.co.jp>
2021-08-17 20:06:05 +0900

    WEBrick dynamicpage

--- webrick.rb
+++ webrick.rb
@@ -1,10 +1,20 @@
 # webrick.rb
 require 'webrick'
+require 'date'
 
 server = WEBrick::HTTPServer.new({ 
   :DocumentRoot => './',
   :BindAddress => '127.0.0.1',
   :Port => 8000
 })
+
+server.mount_proc("/time") do |req, res|
+  # レスポンス内容を出力
+  time = DateTime.now
+  body = time.to_s
+  res.status = 200
+  res['Content-Type'] = 'text/html'
+  res.body = body
+end
 
 server.start
