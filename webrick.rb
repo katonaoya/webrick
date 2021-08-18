a326021cd3768b062a5269b1f5bed77776986c7e
katonaoya <kato7080220@yahoo.co.jp>
2021-08-18 18:18:50 +0900

    Webserver-form

--- webrick.rb
+++ webrick.rb
@@ -16,4 +16,31 @@ server.mount_proc("/time") do |req, res|
   res.body = body
 end
 
+server.mount_proc("/form_get") do |req, res|
+  body = "<head>
+          <meta charset='utf-8'>
+          </head>
+          <html><body>
+          <p>クエリパラメータは#{req.query}です。</p>
+          <p>こんにちは#{req.query["username"]}さん。あなたの年齢は#{req.query["userage"]}ですね。</p>
+          </body></html>"
+  res.status = 200
+  res['Content-Type'] = 'text/html'
+  res.body = body
+end
+
+server.mount_proc("/form_post") do |req, res|
+  # レスポンス内容を出力
+body = "<head>
+          <meta charset='utf-8'>
+          </head>
+          <html><body>
+          <p>フォームデータは#{req.query}です。</p>
+          <p>こんにちは#{req.query["username"]}さん。あなたの年齢は#{req.query["userage"]}ですね。</p>
+          </body></html>"
+  res.status = 200
+  res['Content-Type'] = 'text/html'
+  res.body = body
+end
+
 server.start
